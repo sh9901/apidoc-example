64ed2908ed47884bd8c94080e2e2008ee4f09fa5
huawei.yang <huawei.yang@ecarx.com.cn>
2020-10-14 21:03:49 +0800

    add knife4j support

--- complete/src/main/java/com/example/RestServiceApplication.java
+++ complete/src/main/java/com/example/RestServiceApplication.java
@@ -1,11 +1,13 @@
 package com.example;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
+@EnableKnife4j
 @SpringBootApplication
 public class RestServiceApplication {
 
