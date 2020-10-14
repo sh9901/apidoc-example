852cceffb7d58307585d9b913b5164e3bb600e04
huawei.yang <huawei.yang@ecarx.com.cn>
2020-10-14 11:45:43 +0800

    swagger annotation 示例
    swagger-annotations 不对bean validation起任何作用
    swagger-annotations 可以定义额外的信息，如 example, allowableValues, 这些信息
    是为了生成文档说明，并不在validation中起作用

--- complete/src/main/java/com/example/vo/request/UserFilterRequest.java
+++ complete/src/main/java/com/example/vo/request/UserFilterRequest.java
@@ -7,13 +7,18 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 @Data
+@ApiModel(description = "用户筛选请求")
 public class UserFilterRequest {
     @Min(value = 0, message = "女")
     @Max(value = 1, message = "男")
-    private Integer gender;
+    @ApiModelProperty(required = true, example = "1", allowableValues = "0,1")
+    private int gender;
 
     @NotNull(message = "筛选用户名不可以为空")
+    @ApiModelProperty(required = true)
     private String userName;
 }
